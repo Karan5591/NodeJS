3a56d6dd53ab979a7ba45b4b4c2f4bfc9dde7361
Karan5591 <karansingh5591>
2023-01-26 11:23:58 +0530

    Express Demo

---
+++ app1.js
@@ -0,0 +1,15 @@
+const http=require('http');
+const express=require ('express');
+const app= express();
+
+app.use((req, res, next)=>{
+    console.log("In the middleware");
+    next();
+})
+app.use((req, res, next)=>{
+    console.log("In other Middleware");
+    res.send('<h1> Hello from express</h1>');
+    
+})
+const server=http.createServer(app);
+server.listen(3000);
